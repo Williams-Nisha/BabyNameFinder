1184ea4ed08507f836d1b26429fb129c128fb971
Nisha <Nisha@NishaWilliams>
2015-06-30 21:08:01 -0600

    updated files

--- index.php
+++ index.php
@@ -7,24 +7,28 @@
         <meta name="description" content="This app is a baby name app that we created for our CIT 261 class." >
         <title>Baby Name Finder</title>
         <link type="text/css" media="screen" href="css/styles.css" rel="stylesheet">
+
     </head>
     <body>
         <div class="container">
             <header>
-                <?php include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
+                <?php
+                        include $_SERVER['DOCUMENT_ROOT'] . '/modules/header.php'; ?>
             </header>
             <main>
                 <h1>
                     Baby Name Finder
                 </h1>
+
                 <div class="btn_container aligncenter">
-                    <div class="button caption">Boy Names</div>
-                    <div class="button1 caption">Girl Names</div>
+                    <div class="button caption" onclick="getBoyNames()">Boy Names</div>
+                    <div class="button1 caption" onclick="getGirlNames()">Girl Names</div>
                     <div class="clearfloat"></div>
-                    <div class="button2 caption">Top 50 Names</div>
-                    <div class="button3 caption">Gender Neutral</div>
+                    <div class="button2 caption" onclick="getTop50()">Top 50 Names</div>
+                    <div class="button3 caption" onclick="getGenderNeutral()">Gender Neutral</div>
                     <div class="clearfloat"></div>
                 </div>
+
             </main>
 
             <footer>
@@ -34,4 +38,6 @@
     </body>
 
 </html>
+
+
 
--- landingpage.php
+++ landingpage.php
@@ -30,6 +30,33 @@
                 xmlhttp.open("GET", "index.php", true);
                 xmlhttp.send();
             }
+            var baby_names = [];
+            baby_names[0] = {name: "Emma", sex: "F", url: "landing.php", popularity: 20799};
+            baby_names[1] = {name: "Olivia", sex: "F", url: "landing.php", popularity: 19674};
+            baby_names[2] = {name: "Sophia", sex: "F",url: "landing.php", popularity: 18490};
+            baby_names[3] = {name: "Noah", sex: "M", url: "landing.php", popularity: 19144};
+            baby_names[4] = {name: "Liam", sex: "M",url: "landing.php", popularity: 18342};
+
+            function getBoyNames() {
+            var sortBoyNames = boy_names(baby_names);
+            JSON.parse(document.getElementById("button").innerHTML= sortBoyNames);
+    
+            }
+                function boy_names() {
+                var boy_names;
+                var i;
+                var list = "<ul>";
+                for (i = 0; i < baby_names.length; i++) {
+                    if ((baby_names[i].sex) == "M") {
+                boy_names += '<li><a href="' + baby_names[i].url + '">' +
+                        baby_names[i].name + '</a></li><br>';
+                } else {
+                console.log("There are no boy names available.");
+                }
+                list = "</ul>";
+                return boy_names;
+            }
+        }
         </script>
     </head>
     <body>
@@ -40,7 +67,7 @@
                 </p>
                 <h3>Click to Start</h3>
                 <img class="logobtn" src="/images/logo.png" alt="baby name logo" onclick="getIndex()">
-                <div id="result1" align="center"></div>
+                <div id="button" align="center"></div>
             </main>
 
         </div>
