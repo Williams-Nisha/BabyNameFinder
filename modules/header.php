5e631517d28eacae6784bcc4eafb6cc9c51b4a7b
Vehikite-John <jdavet1244@gmail.com>
2015-07-09 10:38:11 -0700

    Corrected nav code

--- modules/header.php
+++ modules/header.php
@@ -3,13 +3,13 @@
 <div class="logo"><a href="/"><img id="logo" src="/images/logo1.png" alt="logo for baby name app" height="100" width="100"></a></div>
         <div class="tagline">Baby Name Finder</div>
         <div class="clearfloat"></div>
-        <nav>
-            <ul>
-                <li><a href="/home.php">Home</a></li>
-                <li><a href=# onclick="getBoyNames()">Boy Names</a></li>
-                <li><a href=# onclick="getGirlNames()">Girl Names</a></li>
-                <li><a href=# onclick="getTop50()">Top 50 Names</a></li>
-                <li><a href=# onclick="getGenderNeutral()">Gender Neutral</a></li>
+        <nav class="nav">
+            <ul class="nav-list">
+                <li class="nav-link"><a href="/home.php">Home</a></li>
+                <li class="nav-link"><a href=# onclick="getBoyNames()">Boy Names</a></li>
+                <li class="nav-link"><a href=# onclick="getGirlNames()">Girl Names</a></li>
+                <li class="nav-link"><a href=# onclick="getTop50()">Top 50 Names</a></li>
+                <li class="nav-link"><a href=# onclick="getGenderNeutral()">Gender Neutral</a></li>
             </ul>   
         </nav>
 
