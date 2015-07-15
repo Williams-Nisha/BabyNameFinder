48ef9cca7c7a7be079c96c027bda031d7484469d
Nisha <Nisha@NishaWilliams>
2015-07-14 19:26:59 -0600

    updates

--- description.php
+++ description.php
@@ -24,14 +24,18 @@
                 <div class="btn_container aligncenter">
                     <div class="favorites" onclick="viewNames()"><p class="txt">List</p></div>
                     <div class="clearfloat"></div>
-                    <div class="babyDetails">
-                        <h4>Description:</h4> <span class="description"></span>
+                    <div class="babyDetails"><br>
+                        <h4>Description:</h4> <span class="description"></span><br>
                         <h4>Meaning:</h4> <span class="meaning"></span> <br>
-                        <h4>Origin:</h4> <span class="origin"></span>
+                        <h4>Origin:</h4> <span class="origin"></span><br>
                     </div>
-                    <div class="button2 caption" onclick="deleteName()">Delete Name</div>
+                    <div class="clearfloat"></div>
+                    <h4>Create a list of your favorite names.</h4>
+
+                    <div class="button2 caption" onclick="getBoyNames()">Return to Boy Names</div>
                     <div class="button4 caption" onclick="addName()">Add Name</div>
                     <div class="clearfloat"></div>
+                    <div  id="search"><a href="/">Search Again</a></div>                  
                 </div>
 
             </main>
