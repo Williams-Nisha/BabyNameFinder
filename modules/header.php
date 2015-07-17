8df77a64e3e7aaa30fba3086965fb511522cf06f
Nisha <Nisha@NishaWilliams>
2015-07-16 20:14:21 -0600

    Merge origin/master
    Conflicts:
    	src/byui/cit260/secretsoftheSea/view/MoveToNewLocationView.java

--- modules/header.php
+++ modules/header.php
@@ -1,10 +1,17 @@
 
-
 <div class="logo"><a href="/"><img id="logo" src="/images/logo1.png" alt="logo for baby name app" height="100" width="100"></a></div>
         <div class="tagline">Baby Name Finder</div>
         <div class="clearfloat"></div>
         <nav class="nav">
             <ul class="nav-list">
+                <li class="nav-link"><a href="/">Home</a></li>
+            </ul>   
+        </nav>
+        <div class="clearfloat"></div>
+
+<!--      Navigation code for future use
+<nav class="nav">
+           <ul class="nav-list">
                 <li class="nav-link"><a href="/">Home</a></li>
                 <li class="nav-link"><a href=# onclick="getBoyNames()">Boy Names</a></li>
                 <li class="nav-link"><a href=# onclick="getGirlNames()">Girl Names</a></li>
@@ -13,4 +20,5 @@
             </ul>   
         </nav>
         <div class="clearfloat"></div>
-
+-->        
+ 
